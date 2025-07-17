277b1b2aca556cb64a30aa21d380c2c6748ad55a
|_| <yong090617qp@gmail.com>
2025-07-17 16:02:18 +0900

    최종점검

--- script.js
+++ script.js
@@ -1,10 +1,327 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const bestCategoryItems = document.querySelectorAll('.best-category-item');
+const img3 = "http://localhost:3845/assets/79490c73ea39e2642fa235552f68f5accbd96827.png";
+const img4 = "http://localhost:3845/assets/ff56dc3d495a24f460f12d233eeb472937243e14.png";
+const img5 = "http://localhost:3845/assets/b286e559b114ae768388be37a791e8381725c7d2.png";
+const img6 = "http://localhost:3845/assets/0ebdff57f3c453d38e755b852b4899676de7a203.png";
+const img7 = "http://localhost:3845/assets/3c99e487ca72090e30c9c769834691154d646d1e.png";
+const img8 = "http://localhost:3845/assets/e18de1c264a104f34765bc87f60329bd6995e329.png";
+const img9 = "http://localhost:3845/assets/afae90758ec537c467d6e499d9e31b62270e5bed.png";
+const img10 = "http://localhost:3845/assets/668aa3a06a974353830bda5b301e1afb8ec92258.png";
+const img11 = "http://localhost:3845/assets/51422f1d02eae0b345fde0a78f8ebf9465c2413f.png";
+const img12 = "http://localhost:3845/assets/95d20ca544af97c9a20cc1b33006d028d0e4958b.png";
+const img = "http://localhost:3845/assets/763165f52088f4cdd2fabc34662c9267388dba65.svg";
+const img1 = "http://localhost:3845/assets/31bbdbfa01acfca48e68608f4aa0307a207c5f52.svg";
+const imgGroup1 = "http://localhost:3845/assets/b4da223ab6d652408f9b96fe463a665a6c0672a6.svg";
+const img2 = "http://localhost:3845/assets/47ea271e6179cb9afb5f011b213c5150641b79e5.svg";
 
-    bestCategoryItems.forEach(item => {
-        item.addEventListener('click', () => {
-            bestCategoryItems.forEach(i => i.classList.remove('active'));
-            item.classList.add('active');
-        });
-    });
-});
+function Component() {
+  const element = document.createElement('div');
+  element.className = "bg-[#ffffff] relative size-full";
+  element.setAttribute("data-name", "홈페이지");
+  element.id = "node-2001_130";
+
+  element.innerHTML = `
+    <div class="flex flex-col items-center relative size-full">
+        <div class="box-border content-stretch flex flex-col gap-2.5 items-center justify-start px-1.5 py-[5px] relative size-full">
+          <div
+            class="grid-cols-[max-content] grid-rows-[max-content] inline-grid leading-[0] place-items-start relative shrink-0"
+            id="node-2021_447"
+          >
+            <div
+              class="[grid-area:1_/_1] h-[157px] ml-0 mt-0 relative w-[1443px]"
+              data-name="네이게이션"
+              id="node-2014_138"
+            >
+              <div
+                class="absolute contents left-[11px] top-[7px]"
+                data-name="로그"
+                id="node-2014_18"
+              >
+                <div
+                  class="absolute h-[110.213px] left-[38.867px] top-[7px] w-[111.469px]"
+                  data-name="매인아이콘"
+                  id="node-2014_16"
+                >
+                  <img alt class="block max-w-none size-full" src="${img1}" />
+                </div>
+                <div
+                  class="absolute font-['Inter:Semi_Bold',_sans-serif] font-semibold h-[37.787px] leading-[0] left-[11px] not-italic text-[#000000] text-[30px] text-left top-[127px] w-[168px]"
+                  id="node-2014_17"
+                >
+                  <p class="block leading-[normal]">Pet Market</p>
+                </div>
+              </div>
+              <div
+                class="absolute contents top-[55px] translate-x-[-50%]"
+                data-name="검색바"
+                id="node-2014_23"
+                style="left: calc(50% + 47.5px)"
+              >
+                <div
+                  class="absolute h-[51px] rounded-[20px] top-[55px] translate-x-[-50%] w-[562px]"
+                  id="node-2014_19"
+                  style="left: calc(50% + 47.5px)"
+                >
+                  <div class="absolute border-[#ffcc00] border-[5px] border-solid inset-0 pointer-events-none rounded-[20px]" />
+                </div>
+                <div
+                  class="absolute size-[18px] top-[72px] translate-x-[-50%]"
+                  id="node-2014_22"
+                  style="left: calc(50% + 291.5px)"
+                >
+                  <div class="absolute bottom-[-5.556%] left-0 right-[-5.556%] top-0">
+                    <img
+                      alt
+                      class="block max-w-none size-full"
+                      src="${imgGroup1}"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div
+                class="absolute contents right-[17px] top-[22px]"
+                data-name="로그인 및 등등 바"
+                id="node-2014_137"
+              >
+                <div
+                  class="absolute contents right-[207px] top-[22px]"
+                  data-name="회원가입 버튼"
+                  id="node-2014_104"
+                >
+                  <div
+                    class="absolute bg-[#ffffff] h-[29.128px] right-[207px] rounded-[5px] top-[22px] w-[71px]"
+                    id="node-2014_25"
+                  >
+                    <div class="absolute border border-[#000000] border-solid inset-0 pointer-events-none rounded-[5px]" />
+                  </div>
+                  <div
+                    class="absolute font-['Inter:Semi_Bold',_'Noto_Sans_KR:Bold',_sans-serif] font-semibold h-[17px] leading-[0] not-italic right-[270px] text-[#000000] text-[15px] text-left top-7 translate-x-[100%] w-14"
+                    id="node-2014_101"
+                  >
+                    <p class="block leading-[normal]">회원가입</p>
+                  </div>
+                </div>
+                <div
+                  class="absolute contents right-[111.312px] top-[22px]"
+                  data-name="로그인 버튼"
+                  id="node-2014_105"
+                >
+                  <div
+                    class="absolute bg-[#ffffff] h-[29px] right-[111.312px] rounded-[5px] top-[22px] w-[70.688px]"
+                    id="node-2014_106"
+                  >
+                    <div class="absolute border border-[#000000] border-solid inset-0 pointer-events-none rounded-[5px]" />
+                  </div>
+                  <div
+                    class="absolute font-['Inter:Semi_Bold',_'Noto_Sans_KR:Bold',_sans-serif] font-semibold h-[17px] leading-[0] not-italic right-[168px] text-[#000000] text-[15px] text-left top-7 translate-x-[100%] w-[46px]"
+                    id="node-2014_107"
+                  >
+                    <p class="block leading-[normal]">로그인</p>
+                  </div>
+                </div>
+                <div
+                  class="absolute contents right-[17px] top-[22px]"
+                  data-name="고객센터 버튼"
+                  id="node-2014_109"
+                >
+                  <div
+                    class="absolute bg-[#ffffff] h-[29px] right-[17px] rounded-[5px] top-[22px] w-[69px]"
+                    id="node-2014_110"
+                  >
+                    <div class="absolute border border-[#000000] border-solid inset-0 pointer-events-none rounded-[5px]" />
+                  </div>
+                  <div
+                    class="absolute font-['Inter:Semi_Bold',_'Noto_Sans_KR:Bold',_sans-serif] font-semibold leading-[0] not-italic right-[79px] text-[#000000] text-[15px] text-left text-nowrap top-7 translate-x-[100%]"
+                    id="node-2014_111"
+                  >
+                    <p class="block leading-[normal] whitespace-pre">
+                      고객센터
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="bg-[#000000] h-12 relative rounded-[5px] shrink-0 w-[1163px]"
+            data-name="카테고리바"
+            id="node-2021_675"
+          >
+            <div
+              class="absolute h-[38px] left-[610px] top-[5px] w-0"
+              data-name="카테고리"
+              id="node-2021_680"
+            >
+              <div class="absolute bottom-[-6.579%] left-[-2.5px] right-[-2.5px] top-[-6.579%]">
+                <img alt class="block max-w-none size-full" src="${img2}" />
+              </div>
+            </div>
+            <button
+              class="absolute block cursor-pointer h-11 left-px overflow-visible top-0.5 w-[191px]"
+              data-name="Component 26"
+              id="node-2033_1824"
+            >
+            </button>
+            <div
+              class="absolute cursor-pointer h-11 left-[203px] top-0.5 w-[123px]"
+              data-name="Component 26"
+              id="node-2033_1842"
+            >
+            </div>
+            <div
+              class="absolute cursor-pointer h-11 left-[337px] top-0.5 w-[123px]"
+              data-name="Component 26"
+              id="node-2033_1856"
+            >
+            </div>
+            <div
+              class="absolute cursor-pointer h-11 left-[471px] top-0.5 w-[123px]"
+              data-name="Component 26"
+              id="node-2033_1870"
+            >
+            </div>
+            <div
+              class="absolute h-11 left-[628px] top-0.5 w-[123px]"
+              data-name="Component 26"
+              id="node-2033_1884"
+            >
+            </div>
+            <div
+              class="absolute cursor-pointer h-11 left-[760px] top-0.5 w-[123px]"
+              data-name="Component 27"
+              id="node-2033_1898"
+            >
+            </div>
+            <div
+              class="absolute cursor-pointer h-11 left-[891.5px] top-0.5 w-[123px]"
+              data-name="Component 26"
+              id="node-2042_227"
+            >
+            </div>
+            <div
+              class="absolute h-11 left-[1024.5px] top-0.5 w-[123px]"
+              data-name="Component 26"
+              id="node-2033_1937"
+            >
+            </div>
+          </div>
+          <div
+            class="box-border content-stretch flex flex-col gap-2.5 h-[326px] items-center justify-center p-0 relative shrink-0 w-[1500px]"
+            data-name="1"
+            id="node-2014_146"
+          >
+            <div
+              class="font-['Inter:Semi_Bold',_'Noto_Sans_KR:Bold',_sans-serif] font-semibold leading-[0] not-italic relative shrink-0 text-[#000000] text-[30px] text-left text-nowrap"
+              id="node-2014_36"
+            >
+              <p class="block leading-[normal] whitespace-pre">
+                NEW 신상품
+              </p>
+            </div>
+            <div
+              class="cursor-pointer h-[29px] relative shrink-0 w-[69px]"
+              data-name="Component 13"
+              id="node-2029_1162"
+            >
+            </div>
+            <div
+              class="basis-0 box-border content-stretch flex flex-row gap-2.5 grow items-center justify-center min-h-px min-w-px p-0 relative shrink-0 w-full"
+              id="node-2021_430"
+            >
+              <div
+                class="basis-0 bg-center bg-cover bg-no-repeat grow h-full min-h-px min-w-px shrink-0"
+                data-name="상품 및 설명"
+                id="node-2014_121"
+                style="background-image: url('${img3}')"
+              />
+              <div
+                class="basis-0 bg-center bg-cover bg-no-repeat grow h-full min-h-px min-w-px shrink-0"
+                data-name="상품 및 설명"
+                id="node-2014_122"
+                style="background-image: url('${img4}')"
+              />
+              <div
+                class="basis-0 bg-center bg-cover bg-no-repeat grow h-full min-h-px min-w-px shrink-0"
+                data-name="상품 및 설명"
+                id="node-2014_123"
+                style="background-image: url('${img5}')"
+              />
+              <div
+                class="basis-0 bg-center bg-cover bg-no-repeat grow h-full min-h-px min-w-px shrink-0"
+                data-name="상품 및 설명"
+                id="node-2014_124"
+                style="background-image: url('${img6}')"
+              />
+              <div
+                class="basis-0 bg-center bg-cover bg-no-repeat grow h-full min-h-px min-w-px shrink-0"
+                data-name="상품 및 설명"
+                id="node-2014_125"
+                style="background-image: url('${img7}')"
+              />
+            </div>
+          </div>
+          <div
+            class="box-border content-stretch flex flex-col gap-[9px] items-center justify-start p-0 relative shrink-0"
+            data-name="2"
+            id="node-2042_385"
+          >
+            <div
+              class="font-['Inter:Semi_Bold',_'Noto_Sans_KR:Bold',_sans-serif] font-semibold h-[42.256px] leading-[0] not-italic relative shrink-0 text-[#000000] text-[30px] text-left w-[219px]"
+              id="node-2014_38"
+            >
+              <p class="block leading-[normal]">Best 베스트 상품</p>
+            </div>
+            <div
+              class="h-[43px] relative shrink-0 w-[697px]"
+              data-name="Component 11"
+              id="node-2029_983"
+            >
+            </div>
+            <div
+              class="box-border content-stretch flex flex-col gap-2.5 items-start justify-start p-0 relative shrink-0"
+              id="node-2042_383"
+            >
+              <div
+                class="box-border content-stretch flex flex-row gap-[50px] h-[300px] items-center justify-start p-0 relative shrink-0 w-[1500px]"
+                id="node-2042_386"
+              >
+                <div
+                  class="basis-0 bg-center bg-cover bg-no-repeat grow h-full min-h-px min-w-px shrink-0"
+                  data-name="2등 상품 및 설명"
+                  id="node-2042_381"
+                  style="background-image: url('${img8}')"
+                />
+                <div
+                  class="basis-0 bg-center bg-cover bg-no-repeat grow h-full min-h-px min-w-px shrink-0"
+                  data-name="2등 상품 및 설명"
+                  id="node-2014_183"
+                  style="background-image: url('${img9}')"
+                />
+                <div
+                  class="basis-0 bg-center bg-cover bg-no-repeat grow h-full min-h-px min-w-px shrink-0"
+                  data-name="1등 삼품 및 설명"
+                  id="node-2014_191"
+                  style="background-image: url('${img10}')"
+                />
+                <div
+                  class="basis-0 bg-center bg-cover bg-no-repeat grow h-full min-h-px min-w-px shrink-0"
+                  data-name="2등 상품 및 설명"
+                  id="node-2014_169"
+                  style="background-image: url('${img11}')"
+                />
+                <div
+                  class="basis-0 bg-center bg-cover bg-no-repeat grow h-full min-h-px min-w-px shrink-0"
+                  data-name="상품 및 설명"
+                  id="node-2014_171"
+                  style="background-image: url('${img12}')"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  `;
+
+  return element;
+}
+
+document.getElementById('root').appendChild(Component());
